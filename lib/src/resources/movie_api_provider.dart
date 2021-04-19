505665910f5cbd9bb9ecc9046634c1e44299dc15
chakravarthi <chakravarthiviswanath@gmail.com>
2021-04-19 15:28:49 +0530

    changes\

--- lib/src/resources/movie_api_provider.dart
+++ lib/src/resources/movie_api_provider.dart
@@ -1,8 +1,10 @@
 import 'package:bloc/src/models/item_model.dart';
+import 'package:bloc/src/models/trailer_model.dart';
 import 'dart:async';
 import 'movie_provider.dart';
 
 class Repository {
   final moviesApiProvider = MovieApiProvider();
   Future<ItemModel> fetchAllMovies() => moviesApiProvider.fetchMovieList();
+  Future<TrailerModel> fetchTrailers(int movieId) => moviesApiProvider.fetchTrailer(movieId);
 }
--- lib/src/resources/movie_provider.dart
+++ lib/src/resources/movie_provider.dart
@@ -2,22 +2,32 @@ import 'dart:async';
 import 'package:http/http.dart' show Client;
 import 'dart:convert';
 import '../models/item_model.dart';
+import '../models/trailer_model.dart';
 
 class MovieApiProvider {
   Client client = Client();
-  final _apiKey = "c29c916b2e83d23a6d4172c3034b6046";
+  final _apiKey = 'c29c916b2e83d23a6d4172c3034b6046';
+  final _baseUrl = "http://api.themoviedb.org/3/movie";
 
   Future<ItemModel> fetchMovieList() async {
-    print("entered");
-    final response = await client.get(Uri.parse("http://api.themoviedb.org/3/movie/popular?api_key=${_apiKey}"));
-    print(response.body.toString());
-    print(response.statusCode);
+    final response = await client.get(Uri.parse("$_baseUrl/popular?api_key=$_apiKey"));
     if (response.statusCode == 200) {
       // If the call to the server was successful, parse the JSON
       return ItemModel.fromJson(json.decode(response.body));
     } else {
       // If that call was not successful, throw an error.
       throw Exception('Failed to load post');
+    }
+  }
+
+  Future<TrailerModel> fetchTrailer(int movieId) async {
+    final response =
+        await client.get(Uri.parse("$_baseUrl/$movieId/videos?api_key=$_apiKey"));
+
+    if (response.statusCode == 200) {
+      return TrailerModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load trailers');
     }
   }
 }
